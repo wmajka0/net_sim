f762792b8bb39819b6eea73fa531e34ff36f3f91
Wiktor Majka <wmajka@student.agh.edu.pl>
2024-12-22 22:22:05 +0100

    Usunięcie plików cmake i ide z repo

--- cmake-build-debug/googletest-master/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/cLEW/CLionProjects/googletest-master
-# Build directory: E:/cLEW/CLionProjects/MOJEZ_NET_SIM/cmake-build-debug/googletest-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- cmake-build-debug/googletest-master/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/cLEW/CLionProjects/googletest-master/googletest
-# Build directory: E:/cLEW/CLionProjects/MOJEZ_NET_SIM/cmake-build-debug/googletest-master/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
