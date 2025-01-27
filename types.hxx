f091397c4254a2dd073f19dca89cbb604f229902
Jan Majerczyk <167908274+Meloniaszbuk@users.noreply.github.com>
2025-01-27 14:35:38 +0100

    Create types.hxx

---
+++ types.hxx
@@ -0,0 +1,16 @@
+#ifndef NETSIM_TYPES_HPP
+#define NETSIM_TYPES_HPP
+
+#include <functional>
+
+using ElementID = unsigned;
+using TimeOffset = unsigned;
+using Time = unsigned;
+
+using Time = unsigned;
+
+using TimeOffset = Time;
+
+using ProbabilityGenerator = std::function<double()>;
+
+#endif 
